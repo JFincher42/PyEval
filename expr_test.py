573d988a6bec7af168198d92dd47fca8287e3e68
Jon Fincher <jfincher42@gmail.com>
2018-10-30 10:05:25 -0500

    Added unit tests and test setup for VS Code

--- expr_test.py
+++ expr_test.py
@@ -1,4 +1,83 @@
+'''
+Test cases for PyEval
+
+Jon Fincher, July 2018
+'''
+import os
+import unittest
 from pyeval_expression import Expression
 
-expr = Expression("53 *  -4 +2 *  3  ")
-print(expr.result())
+class TestPyEval(unittest.TestCase):
+
+    '''
+    test_positive_operand_expression - tests a correct result from a single positive operand expression
+    '''
+    def test_positive_operand_expression(self):
+        expr = Expression("53")
+        self.assertEqual("53 ", expr.result(), "ERROR: Positive operand")
+
+    '''
+    test_megative_operand_expression - tests a correct result from a single negative operand expression
+    '''
+    def test_negative_operand_expression(self):
+        expr = Expression("-53")
+        self.assertEqual("-53 ", expr.result(), "ERROR: Negative operand")
+
+    '''
+    test_double_term_expression - tests a correct result from a set of double term expressions
+    '''
+    def test_double_term_expression(self):
+        expr = Expression("53+2")
+        self.assertEqual("53 2 + ", expr.result(), "ERROR: Double positive term expression")
+        expr = Expression("-53+2")
+        self.assertEqual("-53 2 + ", expr.result(), "ERROR: Negative/positive term expression")
+        expr = Expression("53+-2")
+        self.assertEqual("53 -2 + ", expr.result(), "ERROR: Positive/negative term expression")
+        expr = Expression("-53+-2")
+        self.assertEqual("-53 -2 + ", expr.result(), "ERROR: Double negative term expression")
+
+    '''
+    test_double_term_operands - tests a correct result from a set of operands
+    '''
+    def test_double_term_operands(self):
+        expr = Expression("53+2")
+        self.assertEqual("53 2 + ", expr.result(), "ERROR: Additive expression")
+        expr = Expression("53-2")
+        self.assertEqual("53 2 - ", expr.result(), "ERROR: Subtrative expression")
+        expr = Expression("53*2")
+        self.assertEqual("53 2 * ", expr.result(), "ERROR: Multiplicative expression")
+        expr = Expression("53/2")
+        self.assertEqual("53 2 / ", expr.result(), "ERROR: Divide expression")
+
+    '''
+    test_triple_term_expression - tests a correct result from a set of triple term expressions
+    '''
+    def test_triple_term_expression(self):
+        expr = Expression("53+2+37")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: Add/Add expression")
+        expr = Expression("53+2*37")
+        self.assertEqual("53 2 37 * + ", expr.result(), "ERROR: Add/Multiply expression")
+        expr = Expression("53*2+37")
+        self.assertEqual("53 2 * 37 + ", expr.result(), "ERROR: Multiply/Add expression")
+        expr = Expression("53*2*37")
+        self.assertEqual("53 2 37 * * ", expr.result(), "ERROR: Multiply/Multiply expression")
+
+    '''
+    test_whitespace_expression - tests a correct result from a variety of whitespace
+    '''
+    def test_whitespace_expression(self):
+        expr = Expression("53+2+37")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: No whitespace expression")
+        expr = Expression("53 + 2 + 37")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: Infixed whitespace expression")
+        expr = Expression(" 53+2+37 ")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: Pre/post-fixed whitespace expression")
+        expr = Expression(" 53 + 2 + 37 ")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: Pre/post/in-fixed whitespace expression")
+        expr = Expression("  53  +  2  +  37  ")
+        self.assertEqual("53 2 37 + + ", expr.result(), "ERROR: Multiple whitespace expression")
+        # This test should fail - spaces in between operands should give an error
+        with self.assertRaises(SyntaxError):
+            expr = Expression("  53  +  -  2  +  37  ")
+            expr.parse()
+
